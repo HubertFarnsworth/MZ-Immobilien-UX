a6b596efb054f4375840aed57a70527ddc47886a
DomiBauer <Dominik@dominiks-mbp-2.fritz.box>
2014-07-17 15:36:41 +0200

    ScrollToTop-Button hinzugefügt

--- src/Startscreen.js
+++ src/Startscreen.js
@@ -23,7 +23,28 @@ Immobilien.Startscreen = (function() {
 		setupAutocompleteWas(); 
         setupTopButtons();
         setupCheckbox();
+        setupScrollButtons(); 
+        
 	},
+
+    setupScrollButtons = function () {
+        $(document).ready(function(){
+    //Check to see if the window is top if not then display button
+            $(window).scroll(function(){
+                if ($(this).scrollTop() > 100) {
+                    $('.scrollToTop').fadeIn();
+                } else {
+                    $('.scrollToTop').fadeOut();
+                }
+            });
+    
+    //Click event to scroll to top
+            $('.scrollToTop').click(function(){
+                $('html, body').animate({scrollTop : 0},800);
+                return false;
+            });
+        });
+    },
 
 	//Funktionen für Mieten/Kaufen-Buttons werden gesetzt
 	createMietenKaufenButton = function () {
