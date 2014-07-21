010aa8c4303c0c03ce540ef43b29b15fee3a6f91
DomiBauer <Dominik@dominiks-mbp-2.fritz.box>
2014-07-21 16:03:13 +0200

    Minimieren bzw. Maximieren der Map hinzugefügt

--- src/Detail.js
+++ src/Detail.js
@@ -1,6 +1,8 @@
 Immobilien.Detail = (function() {
 	var that = {},
 	informations = new Array (), 
+	map = false, 
+
 
 
 	init = function() {
@@ -20,15 +22,62 @@ Immobilien.Detail = (function() {
 		//Sets the Text 
 		informations = data; 
 		setText();
+		setupTopButtons(); 
 
 	    $(document).on("click", "#backToStart", function(event){
       		Immobilien.MainController.init(); 
+    	});
+
+	    //Minimieren bzw. maximieren der Map-Ansicht
+    	$(document).on("click", "#map-size-button", function(event){
+    		if (map === true) {
+    			$("#map-content").height(0);
+      			$("#map-size-button").removeClass("glyphicon-minus");
+      			$("#map-size-button").addClass("glyphicon-plus");
+      			map = false; 
+    		} else {
+    			$("#map-content").height(300);
+    			$("#map-size-button").removeClass("glyphicon-plus");
+      			$("#map-size-button").addClass("glyphicon-minus");
+    			map = true;
+    		}
     	});
 	},
 
 	setText = function () {
 		document.getElementById("immo-headline").innerHTML = informations.rooms + " Zimmer "+ informations.type;
-	};
+	},
+
+	//Setup der Navigationsbuttons
+    setupTopButtons = function () {
+        $("#gesuche-button").attr('class', 'btn btn-primary');
+        $("#anbieten-button").attr('class', 'btn btn-default');
+        $("#merkliste-button").attr('class', 'btn btn-default');
+
+        $("#gesuche-button").click(function() {
+            //user clicks on gesuche-button
+            //give user feedback (change button states)
+            $("#gesuche-button").attr('class', 'btn btn-primary');
+            $("#anbieten-button").attr('class', 'btn btn-default');
+            $("#merkliste-button").attr('class', 'btn btn-default');
+        });
+
+        $("#anbieten-button").click(function() {
+            //user clicks on anbieten-button
+            //give user feedback (change button states)
+            $("#gesuche-button").attr('class', 'btn btn-default');
+            $("#anbieten-button").attr('class', 'btn btn-primary');
+            $("#merkliste-button").attr('class', 'btn btn-default');
+        });
+
+        $("#merkliste-button").click(function() {
+            //user clicks on merkliste-button
+            //give user feedback (change button states)
+            $("#gesuche-button").attr('class', 'btn btn-default');
+            $("#anbieten-button").attr('class', 'btn btn-default');
+            $("#merkliste-button").attr('class', 'btn btn-primary');
+        });
+    };
 
 	that.init = init;
 	that.startDetail = startDetail; 
