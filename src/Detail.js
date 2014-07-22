9b231a50406bdf6e34058a0221f6fd961afa7b59
pinkdroyd <ben@inrane.de>
2014-07-22 13:17:47 +0200

    Sollte Map auf Immobilie zentrieren

--- src/Detail.js
+++ src/Detail.js
@@ -125,27 +125,58 @@ Immobilien.Detail = (function() {
         console.log("map called");
         google.maps.event.addDomListener(window, 'load');
 
-
-
         var address = informations.city + ", " + informations.streetname + " " + informations.housenumber;
         console.log(address);
 
         placeMarkersOnMap(address);
 
+        console.log(getGeoData(address));
+        var locationData = getGeoData(address);
+        var lat = locationData["latitude"];
+        var lng = locationData["longitude"];
+
+        console.log(lat, lng);
+
         var mapOptions = {
-            center: new google.maps.LatLng(49.0167, 11.0833),
-            zoom: 7,
+            center: new google.maps.LatLng(lat, lng),
+            zoom: 12,
         };
 
         globalMap = new google.maps.Map(document.getElementById("map-content"),
                 mapOptions);
     },
 
+    getGeoData = function (address) {
+        geocoder = new google.maps.Geocoder();
+
+        var latitude, longitude;
+        var latlng = new Array();
+
+        geocoder.geocode( { 'address': address}, function(results, status) {
+
+            if (status == google.maps.GeocoderStatus.OK) {
+                latitude = results[0].geometry.location.lat();
+                longitude = results[0].geometry.location.lng();
+
+                console.log(latitude, longitude);
+
+                
+                latlng["0"] = latitude;
+                latlng["1"] = longitude;
+
+                
+            }
+        });
+
+        console.log(latlng);
+        return latlng;
+    },
+
     placeMarkersOnMap = function(address) {
 
         geocoder = new google.maps.Geocoder();
 
-        var latitude, longitude, asd;
+        var latitude, longitude;
 
         geocoder.geocode( { 'address': address}, function(results, status) {
 
